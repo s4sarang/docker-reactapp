f7b351898b5799140c6b68126f4eab843a43b215
Sarang Girdhari <saranggirdhari@gmail.com>
2021-12-01 23:26:06 +0530

    Update App content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hello World, ssup?
+        Hello World, ssup? - On featuure branch!
         </p>
       </header>
     </div>
